14ab9aa4fbfd68726a88f04a2ef92a4feb7796f0
AMRESH <amreshaiio@gmail.com>
2022-07-14 03:41:59 +0530

    v2

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-
-    const url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=74106e93a27465f32815749cba17180e"
-
-    https.get(url, function(response) {
-        // console.log(response, statuscode);           //return status code of api
-        var result='';                                  //empty string
-        response.on("data", function(data){   
-            result+=data;                               //add json string data fetched from api
-        });
-
-        response.on('end', () => {
-        // console.log(result);
-        var weatherData  = JSON.parse(result);           //parsing buffer data to json string
-        // console.log(weatherData);
-        res.write("<h1>RETURNS AN ARRAY OF OBJECTS</h1>"+ weatherData.list);  
-        res.write("<h1>SINGLE OBJECT AT 0</h1>" + weatherData.list[0].main.temp);  
-        //return object at 0 from an array of objects "weatherData" got from api 
-        res.write("<h3>Return NESTED object at 0 from an array of objects weatherData got from api.</h3>")
-        res.send();
-
-        });
-    })
+    res.sendFile(__dirname + "/index.html")
 })
+
+app.post('/', function(req, res) {
+
+    const apikey = "74106e93a27465f32815749cba17180e";
+    const query = req.body.cityName;
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" + apikey +"&units=" + unit
+    console.log(url )
+
+https.get(url, function(response) {
+    // console.log(response, statuscode);           //return status code of api
+    var result='';                                  //empty string
+    response.on("data", function(data){   
+        result+=data;                               //add json string data fetched from api
+    });
+
+    response.on('end', () => {
+    // console.log(result);
+    var weatherData  = JSON.parse(result);           //parsing buffer data to json string
+    // console.log(weatherData);
+    const temp = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;
+    const icon = weatherData.weather[0].icon;
+    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";     
+
+    res.write("<p>The weather is currently "+ weatherDescription + "<p>");  
+    res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");  
+    res.write("<img src=" + imageURL +">")
+    res.send();
+
+    });
+})
+})
+
+
 
 
 
